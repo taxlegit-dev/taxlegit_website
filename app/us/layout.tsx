e45d34aaf4a22173ed2df57d8c9cf28cc552cfa6
taxlegit <developertaxlegit1@gmail.com>
2025-12-26 19:55:12 +0530

    Add dynamic export to layout files and refactor getNavbarItems function

--- app/us/layout.tsx
+++ app/us/layout.tsx
@@ -1,6 +1,8 @@
 import { NavbarServer } from "@/components/navigation/navbar-server";
 import { Region } from "@prisma/client";
 
+export const dynamic = "force-dynamic";
+
 export default function UsLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
