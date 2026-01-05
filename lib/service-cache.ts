572640f29c53dd383b55d78796a6144c91d3f58e
developertaxlegit2 <developertaxlegit2@gmail.com>
2026-01-05 18:35:31 +0530

    revalidate after 60s

--- lib/service-cache.ts
+++ lib/service-cache.ts
@@ -36,7 +36,7 @@ export async function getServiceContentCached(
   const data = { hero, sections, faq };
 
   // 3️⃣ Cache for 24 hours
-  await redis.set(cacheKey, data, { ex: 60 * 60 * 24 });
+  await redis.set(cacheKey, data, { ex: 20});
 
   return data;
 }
