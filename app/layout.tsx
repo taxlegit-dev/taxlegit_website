3ced230734bded7210a1acb49d4a22eb7c52d556
developertaxlegit2 <developertaxlegit2@gmail.com>
2026-01-09 12:25:10 +0530

    chatbot removed temporary

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ProviderTree } from "@/components/providers/provider-tree";
 import "aos/dist/aos.css";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Taxlegit | Compliance and Registration",
@@ -22,18 +21,6 @@ export default async function RootLayout({
         <ProviderTree>
           {children}
         </ProviderTree>
-        <Script id="zoho-init" strategy="afterInteractive">
-          {`
-            window.$zoho = window.$zoho || {};
-            $zoho.salesiq = $zoho.salesiq || { ready: function(){} };
-          `}
-        </Script>
-
-        <Script
-          id="zoho-salesiq"
-          src="https://salesiq.zohopublic.com/widget?wc=siq0e51e73d307d4571c5cde2ed5c2d585af9a0b468d06b8658f9486e3b3d6ff665"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
